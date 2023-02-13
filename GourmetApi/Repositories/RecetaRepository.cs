a644c088b8dc6f4250f929983f55d52d66c5317c
Yesenia López Giraldo <ylopez@hexacta.com>
2023-02-13 09:24:27 -0500

    Repositories

---
+++ GourmetApi/Repositories/RecetaRepository.cs
@@ -0,0 +1,45 @@
+﻿using Gourmet;
+using GourmetApi.Repositories.Contracts;
+
+namespace GourmetApi.Repositories
+{
+    public class RecetaRepository : IRecetaRepository
+    {
+        private DBRecetariosContext context;
+
+        public RecetaRepository(DBRecetariosContext context)
+        {
+            this.context = context;
+        }
+
+        public void DeleteReceta(int recetarioID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Recetario GetRecetaByID(int recetarioId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Recetario> GetRecetas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertReceta(Recetario recetario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateReceta(Recetario recetario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GourmetApi/Repositories/RecetarioRepository.cs
@@ -0,0 +1,66 @@
+﻿using Gourmet;
+using GourmetApi.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace GourmetApi.Repositories
+{
+    public class RecetarioRepository : IRecetarioRepository
+    {
+        private DBRecetariosContext context;
+
+        public RecetarioRepository(DBRecetariosContext context)
+        {
+            this.context = context;
+        }
+
+        public void DeleteRecetario(int recetarioID)
+        {
+            Recetario student = context.Recetarios.Find(recetarioID);
+            context.Recetarios.Remove(student);
+        }
+
+        public Recetario GetRecetarioByID(int id)
+        {
+            return context.Recetarios.Find(id);
+        }
+
+        public IEnumerable<Recetario> GetRecetarios()
+        {
+            return context.Recetarios.ToList();
+        }
+
+        public void InsertRecetario(Recetario recetario)
+        {
+            context.Recetarios.Add(recetario);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void UpdateRecetario(Recetario recetario)
+        {
+            context.Entry(recetario).State = EntityState.Modified;
+        }
+
+        public List<RecetasUnRecetario> RecetasDeUnRecetario(int id)
+        {
+            using var context = new DBRecetariosContext();
+
+            var query = (from receta in context.Recetas
+                         join recetariorecetas in context.RecetasRecetarios
+                         on receta.Id equals recetariorecetas.IdReceta
+                         join recetario in context.Recetarios
+                         on recetariorecetas.IdRecetario equals recetario.Id
+                         where recetariorecetas.IdRecetario == id
+                         select new RecetasUnRecetario
+                         {
+                             IdRecetario = id,
+                             Recetario = recetario.Titulo,
+                             Receta = receta.Titulo
+                         }).ToList();
+            return query;
+        }
+    }
+}
